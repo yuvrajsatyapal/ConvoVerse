696d89e912d2410c00b1cbd2d3a926d352f5f262
yuvrajsatyapal <yuvrajsatyapal21@gmail.com>
2025-07-23 20:41:48 +0530

    Search Bar

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './chatList.css'
 
 const chatList = () => {
+
+  const [addMode, setAddMode] = useState(false);
+
   return (
-    <div className='chatList'>chatList</div>
+    <div className='chatList'>
+      <div className="search">
+        <div className='search-bar'>
+          <img src="/assets/images/search.png" alt="" />
+          <input type="text" placeholder='Search' />
+        </div>
+        <img src={addMode ? "/assets/images/minus.png" : "/assets/images/plus.png"} className='add' alt="" onClick={() => setAddMode((prev) => !prev)} />
+      </div>
+    </div>
   )
 }
 
--- src/components/list/userInfo/UserInfo.jsx
+++ src/components/list/userInfo/UserInfo.jsx
@@ -4,7 +4,15 @@ import './userInfo.css'
 const UserInfo = () => {
   return (
     <div className='userInfo'>
-        <div className="userInfo"></div>
+        <div className="user">
+          <img src="/assets/images/avatar.png" alt="" />
+          <h2>Alex Smith</h2>
+        </div>
+        <div className="icons">
+          <img src="/assets/images/more.png" alt="" />
+          <img src="/assets/images/video.png" alt="" />
+          <img src="/assets/images/edit.png" alt="" />
+        </div>
     </div>
   )
 }
