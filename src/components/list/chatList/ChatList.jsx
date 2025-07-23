8dea6ed5111cfd20d7ccc7284d83f2d66fbda25e
yuvrajsatyapal <yuvrajsatyapal21@gmail.com>
2025-07-23 14:42:20 +0530

    Setting Components

---
+++ src/components/list/chatList/ChatList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './chatList.css'
+
+const chatList = () => {
+  return (
+    <div className='chatList'>chatList</div>
+  )
+}
+
+export default chatList
---
+++ src/components/list/userInfo/UserInfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './userInfo.css'
+
+const UserInfo = () => {
+  return (
+    <div className='userInfo'>
+        <div className="userInfo"></div>
+    </div>
+  )
+}
+
+export default UserInfo
