90aab6334c53a0e1d8c1f34a8a37e9b08bf4167f
yuvrajsatyapal <yuvrajsatyapal21@gmail.com>
2025-07-24 20:22:46 +0530

    UI Updated

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -3,7 +3,22 @@ import './Chat.css'
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className='top'>
+        <div className='user'>
+          <img src="/assets/images/avatar.png" alt="" />
+          <span className="texts">Alex</span>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div className="icons">
+          <img src="/assets/images/phone.png" alt="" />
+          <img src="/assets/images/video.png" alt="" />
+          <img src="/assets/images/info.png" alt="" />
+        </div>
+      </div>
+      <div className='center'></div>
+      <div className='bottom'></div>
+    </div>
   )
 }
 
